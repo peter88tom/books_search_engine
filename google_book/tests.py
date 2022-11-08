29a335fedb383217c1b4feb8fac2e110ef11136b
petro_thomas <peter88tom@gmail.com>
2022-11-09 00:57:37 +0200

    Add test

--- google_book/tests.py
+++ google_book/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from google_book.views import search_books
+from admin.settings import API_KEY
+import requests
 
 # Create your tests here.
+def test_search_books():
+
+  # require parameters
+  end_point = "https://www.googleapis.com/books/v1/volumes?"
+  key_word = "Python"
+
+  response = requests.get(f"{end_point}q={key_word}&key={API_KEY}")
+  assert response.status_code == 200
+
+
